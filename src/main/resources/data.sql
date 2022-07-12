497e10cca7d60abd39b70c0fb6546b4cbb5d268f
keerthi1024 <keerthiemmadi010@gmail.com>
2022-07-12 19:52:44 +0530

    commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO ACCOUNT(account_Id,customer_Id,current_Balance,account_Type,opening_date,owner_Name)
+VALUES (1000000001,'CUSTOMER101',80000.00, 'Savings',TO_DATE('02/07/2022', 'DD/MM/YYYY'), 'Vasavi');
+
+INSERT INTO ACCOUNT(account_Id,customer_Id,current_Balance,account_Type,opening_date,owner_Name)
+VALUES (1000000002, 'CUSTOMER102', 2000.00, 'Current', TO_DATE('03/07/2022', 'DD/MM/YYYY'),'Ananya');
